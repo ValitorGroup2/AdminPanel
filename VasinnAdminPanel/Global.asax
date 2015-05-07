0597c1416d026c4dfc1a60fe75d0a782dd897c87
Grellinn <grellinn@gmail.com>
2015-05-07 15:42:57 +0000

    WebApp with DB connections added.

---
+++ VasinnAdminPanel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VasinnAdminPanel.MvcApplication" Language="C#" %>
---
+++ VasinnAdminPanel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VasinnAdminPanel.Startup))]
+namespace VasinnAdminPanel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
